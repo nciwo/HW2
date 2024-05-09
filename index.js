9e8cb36ad85a4304d9e97ff3093987a84ff39984
nciwo <thebluesky771@gmail.com>
2024-05-10 02:37:40 +0900

    Made first frame of Homework #2

---
+++ index.js
@@ -0,0 +1,11 @@
+const easteregg = document.querySelector("#clickme");
+let click = 0;
+
+easteregg.addEventListener("click", e=> {
+    e.preventDefault();
+    click += 1;
+    if(click == 10) {
+        console.log("Hi.");
+        click = 0;
+    } 
+});
